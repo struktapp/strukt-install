8e5fc7d91abd8c96122337ec10b8f537284d685f
pitsolu <pitsolu@gmail.com>
2023-02-26 19:25:02 +0300

    initial commit

--- src/Installer.php
+++ src/Installer.php
@@ -2,16 +2,10 @@
 
 namespace Strukt\Console\Command;
 
-// use Strukt\Fs;
 use Strukt\Console\Color;
 use Strukt\Console\Input;
 use Strukt\Console\Output;
 use Strukt\Process;
-
-// use Strukt\Env;
-// use Strukt\Fs;
-// use Strukt\Process;
-// use Strukt\Templator as Tpl;
 
 /**
 * new    Install latest Strukt
@@ -28,12 +22,16 @@ class Installer extends \Strukt\Console\Command{
 
 	public function execute(Input $in, Output $out){
 
-		$name = $in->get("app_name");
+		$app_name = $in->get("app_name");
+		if(empty($app_name))
+			throw new \Exception("Argument [app_name] is required!");
 
-		$version = "strukt/strukt:v1.1.5-alpha";
-		$command = sprintf("composer create-project %s --prefer-dist %s",
+		$package = "strukt/strukt";
+		$version = "v1.1.5-alpha";
+		$command = sprintf("composer create-project %s:%s --prefer-dist %s",
+						$package,
 						$version,
-						$name);
+						$app_name);
 
 		Process::switchChannels();
 		$ps = Process::run([$command], function($streamOutput){
