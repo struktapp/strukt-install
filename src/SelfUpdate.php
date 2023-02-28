d79b8cf998b1b0c8e165a4e9ff93dce4a9ee3bdd
pitsolu <pitsolu@gmail.com>
2023-02-28 03:06:26 +0300

    added selfupdate command

---
+++ src/SelfUpdate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Strukt\Console\Command;
+
+use Strukt\Console\Color;
+use Strukt\Console\Input;
+use Strukt\Console\Output;
+use Strukt\Process;
+use Dotenv\Dotenv;
+
+/**
+* selfupdate    Update Strukt Installer
+*/
+class SelfUpdate extends \Strukt\Console\Command{
+
+	public function __construct(){
+
+		$dotenv = Dotenv::createImmutable(__DIR__."/../");
+		$dotenv->load();
+	}
+
+	public function execute(Input $in, Output $out){
+
+		$cmd = "composer update strukt/install --working-dir=../../../"
+
+		Process::switchChannels();
+		$ps = Process::run([$cmd], function($streamOutput){
+
+			echo Color::write("cyan", $streamOutput);
+		});
+	}
+}
